3fbb96032039d2db3e6a681d662b9e62e04c3eb6
Joshua Michael Hertlein <jmhertlein@gmail.com>
2015-02-20 00:35:40 -0600

    Updated the annotation processor for type coercion

--- src/main/java/net/jmhertlein/reflective/processor/CommandMethodProcessor.java
+++ src/main/java/net/jmhertlein/reflective/processor/CommandMethodProcessor.java
@@ -18,39 +18,34 @@ package net.jmhertlein.reflective.processor;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.Types;
 import javax.tools.Diagnostic.Kind;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 
 /**
  *
  * @author joshua
  */
-@SupportedAnnotationTypes("net.jmhertlein.core.ebcf.annotation.CommandMethod")
+@SupportedAnnotationTypes("net.jmhertlein.reflective.annotation.CommandMethod")
 public class CommandMethodProcessor extends AbstractProcessor {
     private static final String ERR_MSG_PARAMS = "CommandMethod-annotated method params must be either (), (CommandSender), (String[]), or (CommandSender,String[])";
     private static final String ERR_MSG_VISIBILITY = "CommandMethod-annotated methods must be public.";
-    
-    private static final Set<List<String>> validParamsLists;
-    
-    static {
-        validParamsLists = new HashSet<>();
-        validParamsLists.add(new ArrayList<>(0));
-        validParamsLists.add(new ArrayList<String>(1) {{ add(CommandSender.class.getCanonicalName()); }});
-        validParamsLists.add(new ArrayList<String>(1) {{ add(String[].class.getCanonicalName()); }});
-        validParamsLists.add(new ArrayList<String>(2) {{ 
-            add(CommandSender.class.getCanonicalName());
-            add(String[].class.getCanonicalName()); 
-        }});
-    }
 
     @Override
     public boolean process(Set<? extends TypeElement> set, RoundEnvironment re) {
@@ -63,14 +58,85 @@ public class CommandMethodProcessor extends AbstractProcessor {
                         if(!executable.getModifiers().contains(Modifier.PUBLIC)) {
                             processingEnv.getMessager().printMessage(Kind.ERROR, ERR_MSG_VISIBILITY, executable);
                         }
-                        
-                        List<String> params = executable.getParameters().stream().map(p -> p.asType().toString()).collect(Collectors.toList());
-                        if(!validParamsLists.stream().anyMatch(goodList -> goodList.equals(params))) {
-                            processingEnv.getMessager().printMessage(Kind.ERROR, ERR_MSG_PARAMS, executable);
-                        }
+
+                        //List<String> params = executable.getParameters().stream().map(p -> p.asType().toString()).collect(Collectors.toList());
+                        checkWellFormedParamList(executable);
                     });
         });
-        
+
         return true;
+    }
+
+    private void checkWellFormedParamList(ExecutableElement e) {
+        List<? extends VariableElement> params = e.getParameters();
+
+        int i = 0;
+        for(VariableElement v : params) {
+            if(isType(v, "org.bukkit.command.CommandSender") || isType(v, "org.bukkit.entity.Player") || isType(v, "org.bukkit.command.ConsoleCommandSender")) {
+                if(i != 0) {
+                    compileError(v, "CommandSender variable must be first in parameter list.");
+                }
+            } else if(isType(v, getArrayMirrorForName("java.lang.String"))) {
+                if(i != (params.size() - 1)) {
+                    compileError(v, "String[] must be the last parameter.");
+                }
+            } else {
+                if(!isAllowedPrimitiveKind(v.asType().getKind())) {
+                    compileError(v, "Cannot automatically convert to non-primitive type \"" + v.asType().toString() + "\"");
+                }
+            }
+
+            i++;
+        }
+    }
+
+    /**
+     * takes the FQN of a type and returns a TypeMirror representing it based on how a name was
+     * mapped to a TypeMirror in the answer to this post:
+     * https://stackoverflow.com/questions/20358039/java-annotations-processor-check-if-typemirror-implements-specific-interface
+     *
+     * @param s
+     * @return
+     */
+    private TypeMirror getMirrorForName(String s) {
+        return processingEnv.getElementUtils().getTypeElement(s).asType();
+    }
+
+    private TypeMirror getArrayMirrorForName(String s) {
+        return processingEnv.getTypeUtils().getArrayType(getMirrorForName(s));
+    }
+
+    /**
+     *
+     * @param v variable's type to check
+     * @param fqn fully qualified type name
+     * @return whether the variable's type is the name as the FQN
+     */
+    private boolean isType(VariableElement v, String fqn) {
+        return processingEnv.getTypeUtils().isSameType(v.asType(), getMirrorForName(fqn));
+    }
+
+    private boolean isType(VariableElement v, TypeMirror type) {
+        return processingEnv.getTypeUtils().isSameType(v.asType(), type);
+    }
+
+    private void compileError(Element e, String msg) {
+        processingEnv.getMessager().printMessage(Kind.ERROR, msg, e);
+    }
+
+    private static boolean isAllowedPrimitiveKind(TypeKind k) {
+        switch(k) {
+            case LONG:
+            case BOOLEAN:
+            case CHAR:
+            case DOUBLE:
+            case FLOAT:
+            case INT:
+            case BYTE:
+            case SHORT:
+                return true;
+            default:
+                return false;
+        }
     }
 }
